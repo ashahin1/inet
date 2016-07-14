95d7094b272344d0b7829e841d3dda7dc9157747
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-07-14 15:06:26 +0200

    udp: fixup ProtocolReq/ProtocolInd tags

--- src/inet/transportlayer/udp/UDP.cc
+++ src/inet/transportlayer/udp/UDP.cc
@@ -736,6 +736,8 @@ void UDP::sendUp(cPacket *payload, SockDesc *sd, const L3Address& srcAddr, ushor
     udpCtrl->setTypeOfService(tos);
     payload->setControlInfo(udpCtrl);
     payload->setKind(UDP_I_DATA);
+    delete payload->removeTag<ProtocolReq>();
+    payload->ensureTag<ProtocolInd>()->setProtocol(&Protocol::udp);
     payload->ensureTag<SocketInd>()->setSocketId(sd->sockId);
     payload->ensureTag<TransportProtocolInd>()->setProtocol(&Protocol::udp);
     payload->ensureTag<L3AddressInd>()->setSource(srcAddr);
@@ -755,6 +757,7 @@ void UDP::sendUpErrorIndication(SockDesc *sd, const L3Address& localAddr, ushort
     udpCtrl->setSrcPort(sd->localPort);
     udpCtrl->setDestPort(remotePort);
     notifyMsg->setControlInfo(udpCtrl);
+    notifyMsg->ensureTag<ProtocolInd>()->setProtocol(&Protocol::udp);
     notifyMsg->ensureTag<SocketInd>()->setSocketId(sd->sockId);
 
     send(notifyMsg, "appOut");
